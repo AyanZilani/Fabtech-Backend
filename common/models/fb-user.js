2382b68352324b77e5699210c8b0bc3c891e2d56
Hamdan Sayuti <superdanstar@gmail.com>
2019-10-24 12:23:41 +0800

    Update

--- common/models/fb-user.js
+++ common/models/fb-user.js
@@ -1,8 +1,29 @@
 'use strict';
 
-module.exports = function(Fbuser) {
+module.exports = function(FbUser) {
 
+    var app;
+
+    FbUser.on('attached', function (a) {
+        app = a;
+    });
     
+    FbUser.afterRemote('login', function (ctx, model, next) {
 
+        // console.log(ctx.result)
+        let RoleMapping = app.models.RoleMapping
+        RoleMapping.findOne({where:{principalId: ctx.result.userId}}, function(err1, result){
+            if (err1) return next(err1)
 
+            console.log(result)
+            let Role = app.models.Role
+            Role.findOne({where:{id:result.roleId}},function(err2,result2){
+                if (err2) return next(err2)
+                console.log(result2)
+                ctx.result.role = result2.name
+                next()
+            })
+        })
+
+    })
 };
